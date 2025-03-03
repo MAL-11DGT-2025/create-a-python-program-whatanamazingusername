c09325540ecc8efefbea8c14a81a527a3f77f868
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-04 10:34:51 +1300

    Asks age

--- Hello.py
+++ Hello.py
@@ -2,6 +2,10 @@
 print("HEEEELLLLLLLOOOOOOO WOOOORRRLLLDDDD")
 print("My favourite colour is clear (Yes clear is a colour -_-)")
 
-    #Asks name
+    #Asks name and says hello
 name = input("Hello new person! What is your name? :o ")
 print(f"Well then hello {name}")
+
+    #Asks age, + uses name and age in sentance
+age = input("How old are you? ^^")
+print(f"Wow {name}, {age} already? Skill issue smh")
