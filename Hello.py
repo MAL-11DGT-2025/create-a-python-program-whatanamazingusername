5a97a74c23d69ea12c8babde8f2e3fd9363cf4d5
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-04 10:25:30 +1300

    Asks name

--- Hello.py
+++ Hello.py
@@ -1,2 +1,7 @@
+    #Intro
 print("HEEEELLLLLLLOOOOOOO WOOOORRRLLLDDDD")
 print("My favourite colour is clear (Yes clear is a colour -_-)")
+
+    #Asks name
+name = input("Hello new person! What is your name? :o ")
+print(f"Well then hello {name}")
