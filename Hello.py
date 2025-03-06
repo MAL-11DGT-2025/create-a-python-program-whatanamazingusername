4e299c5cd747cb0081d2b0c855572865f4e03c51
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-06 14:34:55 +1300

    Added more

--- Hello.py
+++ Hello.py
@@ -1,17 +1,23 @@
     #Intro
-print("HEEEELLLLLLLOOOOOOO WOOOORRRLLLDDDD")
-print("My favourite colour is clear (Yes clear is a colour -_-) ")
+#print("HEEEELLLLLLLOOOOOOO WOOOORRRLLLDDDD")
+#print("My favourite colour is clear (Yes clear is a colour -_-) ")
 
     #Asks name and says hello
-name = input("Hello new person! What is your name? :o \n ")
-print(f"Well then hello {name}")
+#name = input("Hello new person! What is your name? :o \n ")
+#print(f"Well then hello {name}")
 
-    #Asks age, + uses name and age in sentance
-age = int(input("How old are you? ^^ \n"))
-print(f"Wow {name}, you're {age}?? ")
-if age >= 25:
-    print("""OLD ASF LMAO you're NOT simga, you're NOT skibidi, you're NOT the goat, negative aura. 
-I bet you don't even know what those words mean, L bozo \n""")
+    #Asks age, + uses name and age in sentance (Adult, teen, child)
+#age = int(input("How old are you? ^^ \n"))
+#print(f"Wow {name}, you're {age}?? ")
+#if age >= 25:
+#    print("""OLD ASF LMAO you're NOT simga, you're NOT skibidi, you're NOT the goat, negative aura. 
+#I bet you don't even know what those words mean, L bozo \n""")
 
-elif age >= 13 and age <= 19:
-    print("FR FR NO CAP one of the GOATS!! Dap me up my guy")
+#elif age >= 13 and age <= 19:
+#    print("FR FR NO CAP one of the GOATS!! Dap me up my guy, you're a teen just like me frrr")
+
+#elif age < 13:
+#    print("")
+
+
+
