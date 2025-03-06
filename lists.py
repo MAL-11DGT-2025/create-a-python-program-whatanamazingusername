c1f58a9c85c0b77d6e4ddbed96bc4634da975864
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-07 12:53:33 +1300

    made new file, wrote list and # it out

---
+++ lists.py
@@ -0,0 +1,14 @@
+colours = ["red", "green", "blue", "purple", "bleen (blue green)", "clear"]
+
+# for i in colours:
+#     print(i)
+
+# print(colours[-3])
+
+# print(len(colours))
+
+# for i in colours:
+#    if i == "yellow":
+#        print("YELLOOOOOWWWWWW!!!!")
+#    else:
+#        print("Not yellow >:[")
