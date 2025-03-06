4d1c4a659f6e34c7ee5377364c93a7a42e5736dd
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-06 15:15:35 +1300

    Made a story and named veriables

---
+++ story.py
@@ -0,0 +1,12 @@
+def tell_a_story(age, name, car, food, whanau, game):
+
+    print(f"Kia ora {name}! My name is Kinito pet! Haha, I'm kidding :)\n Let me tell you a story {name}, \n \n One upon a time, in a land not so far away, there was a person called {name} who was {age} years old! and they hated {whanau} because they loved {food} >:[ How disgusting!! \n Not only that though, they also drove in {car}s and were sucked at {game}! \n")
+
+name = input("Hello! What is your name? :O \n-")
+age = input("I see! ^^ how old are you? \n-")
+car = input(f"What is your favourite car, {name}> \n-")
+food = input("Ok! Now whats your least favourite food? \n-")
+whanau = input("And whats the worst whanau/house? :) \n-")
+game = input("Alright, last question! What is you favourite game? :D \n-")
+
+tell_a_story(age, name, car, food, whanau, game)
