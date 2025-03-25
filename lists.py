51db1e21eb3ded5282089d0256259c220987612f
whatanamazingusername <chanelle.hamilton@marcellin.school.nz>
2025-03-26 10:03:04 +1300

    added more list related code + comments

--- lists.py
+++ lists.py
@@ -15,16 +15,42 @@
 
 
 colours = ["red", "green", "blue", "purple", "bleen (blue green)", "clear"]
+# print(colours)
 
-print(colours)
+# # adds then prints 5 new colours
+# colours.append("bloop")
+# print(colours)
 
-# adds then prints 3 new colours
-colours.append("bloop")
-print(colours)
+# colours.append("pertangle")
+# print(colours)
 
-colours.append("pertangle")
-print(colours)
+# colours.append("racist green")
+# print(colours)
 
-colours.append("racist green")
-print(colours)
+# colours.append("borange")
+# print(colours)
 
+# colours.append("racist green")
+# print(colours)
+
+# print("Those last 5 are references to something I swear im not insane")
+
+# #prints the amount of colours
+# print(colours.count)
+
+# # prints amount of specidic colurs
+# print(colours.count("racist green"))
+
+# # sorts and prints colours list alphabetacly-----------------------------------
+# colours.sort()
+# print(colours)
+
+# # prints the amount in the list
+# print(len(colours))
+
+# prints specific place in list
+print(colours[1])
+
+#prints all colours
+for colour in colours:
+    print(colour)
